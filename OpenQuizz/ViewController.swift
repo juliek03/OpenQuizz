307ab8d6e6c57f8dc50a92c00bb53df16a4091be
juliek03 <julie-kane@live.fr>
2020-06-19 11:19:12 +0200

    Initial Commit

---
+++ OpenQuizz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenQuizz
+//
+//  Created by Neko on 19/06/2020.
+//  Copyright © 2020 Neko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
