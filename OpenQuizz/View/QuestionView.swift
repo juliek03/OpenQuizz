fe51f9769e96e6881bc0f8df2ff65fc22afc0c90
juliek03 <julie-kane@live.fr>
2020-07-03 13:51:32 +0200

    update

---
+++ OpenQuizz/View/QuestionView.swift
@@ -0,0 +1,16 @@
+//
+//  QuestionView.swift
+//  OpenQuizz
+//
+//  Created by Neko on 01/07/2020.
+//  Copyright © 2020 Neko. All rights reserved.
+//
+
+import UIKit
+
+class QuestionView: UIView {
+    
+    @IBOutlet private var label: UILabel!
+    @IBOutlet private var icon: UIImageView!
+
+}
